f331aea1d4a3f4a0100523c50e80cea63dc7c858
kimtch <tetchan03@gmail.com>
2025-04-01 19:38:42 +0300

    KS-1-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,9 +12,9 @@ fun main() {
     val totalSeconds = 6480
 
     val hours = totalSeconds / (60 * 60)
-    val remainingSeconds = totalSeconds % (60 * 60)
-    val minutes = remainingSeconds / 60
-    val seconds = remainingSeconds % 60
+    val remainingSecond = totalSeconds % (60 * 60)
+    val minutes = remainingSecond / 60
+    val seconds = remainingSecond % 60
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
